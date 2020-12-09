d740b7abbc98d2eace48ee3d9f1ae44e51f4694a
Zhang-Zexi <2445459509@qq.com>
2020-12-09 21:53:55 +0800

    test: test

---
+++ src/main/java/com/test/zzx/Test.java
@@ -0,0 +1,15 @@
+package com.test.zzx;
+
+/**
+ * @ClassName Test
+ * @Description 测试代码提交是否正常
+ * @Author zhangzx
+ * @Date 2020/12/9 21:52
+ * Version 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
